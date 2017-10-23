f1e18a0c1ca8019ab14395531e52fb0a542c5beb
Joe Kim <josephkim0224@gmail.com>
2017-10-23 18:36:33 -0400

    create Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        Some Random Text
+      </div>
+    )
+  }
+}
+
+export default Home
