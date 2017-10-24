d34bc2b766ac652337389a1fffca9d86ef6328cd
Joe Kim <josephkim0224@gmail.com>
2017-10-24 08:54:53 -0400

    add inputs, create change and submit callback, and send data to the backend

--- src/components/presentation/Sidebar.js
+++ src/components/presentation/Sidebar.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react'
+import turbo from 'turbo360'
 
 class Sidebar extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      feed: {
+        name: '',
+        url: ''
+      }
+    }
+  }
+
+  updateFeed(field, event) {
+    let feed = Object.assign({}, this.state.feed)
+    feed[field] = event.target.value
+
+    this.setState({
+      feed: feed
+    })
+  }
+
+  addFeed(event) {
+    event.preventDefault()
+    
+    var turboClient = turbo({site_id: '59ef2f3bfaf4920012e2aa3d'})
+    turboClient.create('feed', this.state.feed)
+    .then(data => {
+      console.log("FEED CREATED: " + JSON.stringify(data))
+    })
+    .catch(err => {
+      alert('Error: ' + err.message)
+    })
+  }
 
   render() {
     return (
       <div id="sidebar">
         <div className="inner">
           <section id="search" className="alt">
-            <form method="post" action="#">
-              <input type="text" name="query" id="query" placeholder="Feed Name" /><br />
-              <input type="text" name="query" id="query" placeholder="Feed URL" /><br />
+            <form onSubmit={this.addFeed.bind(this)} method="post" action="#">
+              <input onChange={this.updateFeed.bind(this, 'name')} type="text" name="query" id="query" placeholder="Feed Name" /><br />
+              <input onChange={this.updateFeed.bind(this, 'url')} type="text" name="query" id="query" placeholder="Feed URL" /><br />
               <button>Add Feed</button>
             </form>
           </section>
